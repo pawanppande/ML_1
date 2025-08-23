6908b65cf3fa59f9cb48ec12fac550e26815f664
pawanppande <pawanppande1496@gmail.com>
2025-08-23 15:20:13 +0530

    logging and exception

---
+++ vene/src/logger.py
@@ -0,0 +1,24 @@
+import logging 
+import os
+from datetime import datetime
+
+LOG_File = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_File)
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_File)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
+    try:
+        a = 1 / 0   
+    except Exception as e:
+        logging.error(e)
+    logging.info("Logging has ended"),
+    python_requires = '>=3.7'
